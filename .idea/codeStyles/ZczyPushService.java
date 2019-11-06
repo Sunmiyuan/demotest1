a4e25a4b7c648f52e3dffb994487632ef62a1231
yuankaixin@zczy56.com <yuankaixin@zczy56.com>
2019-11-06 13:40:28 +0800

    adfaadfdaa

--- .idea/codeStyles/ZczyPushService.java
+++ .idea/codeStyles/ZczyPushService.java
@@ -8,14 +8,6 @@ import com.igexin.sdk.GTIntentService;
 import com.igexin.sdk.GTServiceManager;
 import com.igexin.sdk.PushService;
 
-/**
- * 功能描述:个推服务
- *
- * @author SunFeihu 孙飞虎
- * @company 中储南京智慧物流科技有限公司
- * @copyright （版权）中储南京智慧物流科技有限公司所有
- * @date 2018/12/3
- */
 public class ZczyPushService extends Service {
     //intent:#Intent;action=android.intent.action.oppopush;component=com.tiema.zhwl_android/com.tiema.zhwl_android.MainActivity;S.msgType=0;S.contentId=1;end
     @Override
